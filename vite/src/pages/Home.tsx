1b3626a49aaac7f18244a7c3769965854a1b2a34
Jo-Jae <whwodjs11@naver.com>
2024-06-10 16:33:55 +0900

    Sale Nft Page

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -4,8 +4,16 @@ import unreveal from "../images/unreveal.jpg";
 
 const Home: FC = () => {
   return (
-    <Flex bgColor="red.100" maxW={768} mx="auto" minH="100vh" flexDir="column">
-      <Flex maxW={768} width={768} height={200}>
+    <Flex
+      bgColor="red.100"
+      w="100%"
+      alignItems="center"
+      flexDir="column"
+      gap={2}
+      mt={8}
+      mb={20}
+    >
+      <Flex maxW={768} w="100%" height={200}>
         <img src={unreveal} alt="슬라임이미지" />
       </Flex>
       <Flex>슬라임월드세계관</Flex>
